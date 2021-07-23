f59912d711a7391fd94a669b96d29304968f5926
Abe <bernabe.cambarihanjr@gmail.com>
2021-07-23 10:09:06 +0800

    Initial Commit

---
+++ db/schema.rb
@@ -0,0 +1,14 @@
+ActiveRecord::Schema.define(version: 2021_07_23_010309) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "number"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :contact do
+    name { Faker::Name.name }
+    number { Faker::PhoneNumber.phone_number }
+    email { Faker::Internet.email }
+  end
+end
