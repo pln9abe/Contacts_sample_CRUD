1d2db2e5185ef3aed5650501e6de3db53687ed76
Abe <bernabe.cambarihanjr@gmail.com>
2021-07-23 14:15:01 +0800

    Add update and set before_action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,6 @@
 class ContactsController < ApplicationController
+  before_action :set_contact, only: %i[ show edit update desctroy ]
+
   def index
     @contacts = Contact.all
   end
@@ -7,8 +9,19 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
+  def edit
+  end
+
+  def update
+    if @contact.update(contact_params)
+      redirect_to contacts_path, notice: "Contact successfully updated!"
+    else
+      render :edit
+    end
+  end
+
   def create
-    @contact = Contact.new(contact_params)
+    @contact = Contact.new
 
     if @contact.save
       redirect_to contacts_path, notice: "Contact successfully Added"
@@ -17,6 +30,10 @@ class ContactsController < ApplicationController
     end
   end
   private
+
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
 
   def contact_params
     params.require(:contact).permit(:name, :number, :email)
