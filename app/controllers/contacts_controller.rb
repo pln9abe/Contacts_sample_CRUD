6091cb69dd4fe3117c140f34c98d016bf24920c4
Abe <bernabe.cambarihanjr@gmail.com>
2021-07-23 13:07:04 +0800

    Define Method contact_params

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,12 +8,17 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new
+    @contact = Contact.new(contact_params)
 
     if @contact.save
       redirect_to contacts_path, notice: "Contact successfully Added"
     else 
       render :new
     end
+  end
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :number, :email)
   end
 end
